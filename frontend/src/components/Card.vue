2fc3d50beef21c0ea3477d848c7eedb9f73c59c8
Vadim Goryainov <v.goryainov@tum.de>
2023-02-26 19:40:10 +0100

    added vuetify, first layout

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-card class="card" :text="props.text" variant="outlined" min-width="200px" :hover="true">
+	</v-card>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+	text: string;
+}>()
+</script>
+
+<style scoped>
+	.card {
+		margin: 10px;
+	}
+</style>
